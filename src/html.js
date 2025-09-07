0aff0fa7fa6740bebb16e253e800723931bddf64
Shun Git <kwokshungit@gmail.com>
2025-09-07 15:33:27 +0800

    cleanup

--- src/html.js
+++ src/html.js
@@ -1,4 +1,4 @@
-export const indexHtml = () => /*html*/ `<!DOCTYPE html>
+export const createHtml = (body) => /*html*/ `<!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="UTF-8" />
@@ -6,29 +6,12 @@ export const indexHtml = () => /*html*/ `<!DOCTYPE html>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Singpass demo app</title>
 	</head>
-	<body>
-		<div id="app">
-			<button id="login">login</button>
-		</div>
-		<script type="module">
-		document.querySelector('#login').onclick = async () => {
-			const resp = await fetch('/url')
-			const data = await resp.json()
-			location.href = data.url
-		}
-		</script>
-	</body>
+	<body>${body}</body>
 </html>`
 
-export const callbackHtml = (message) => /*html*/ `<!DOCTYPE html>
-	<head>
-		<meta charset="UTF-8" />
-		<link rel="icon" href="data:null" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>Singpass demo app</title>
-	</head>
-	<body>
-		<div>${message}</div>
-		<a href="/">back to start</a>
-	</body>
-</html>`
+export const indexBody = /*html*/ `<a href="/url">login</a>`
+
+export const callbackBody = (message) => /*html*/ `
+<div>${message}</div>
+<a href="/">back to start</a>
+`
--- src/index.js
+++ src/index.js
@@ -1,46 +1,43 @@
 import { randomUUID, randomBytes } from 'node:crypto'
 
-import { H3, serve, html, useSession, getQuery } from 'h3'
+import { H3, serve, html, useSession, getQuery, redirect } from 'h3'
 import { Issuer, generators } from 'openid-client'
 
 import * as config from './config.js'
-import { indexHtml, callbackHtml } from './html.js'
+import { createHtml, indexBody, callbackBody } from './html.js'
 
 const sigKey = config.PRIVATE_SIG_KEY
 const encKey = config.PRIVATE_ENC_KEY
-
 const redirect_uri = config.REDIRECT_URI
 
 const issuer = await Issuer.discover(config.ISSUER_URL)
 const client = new issuer.Client(
 	{
 		client_id: config.CLIENT_ID,
-		token_endpoint_auth_method: 'private_key_jwt',
 		response_types: ['code'],
+		token_endpoint_auth_method: 'private_key_jwt',
 
+		// values are from Singpass
 		id_token_signed_response_alg: 'ES256',
+		id_token_encrypted_response_enc: 'A256CBC-HS512',
 
 		...(config.USE_MOCKPASS_ISSUER
 			? { id_token_encrypted_response_alg: encKey.alg }
 			: null), // demo app is not encrypting
 
-		id_token_encrypted_response_enc: 'A256CBC-HS512',
-
+		// values are from Singpass
 		userinfo_signed_response_alg: 'ES256',
+		userinfo_encrypted_response_enc: 'A256GCM',
 
 		userinfo_encrypted_response_alg: encKey.alg,
-
-		userinfo_encrypted_response_enc: 'A256GCM',
 	},
 	{ keys: [sigKey, encKey] }
 )
 
 const app = new H3()
 
-const toPublicKey = ({ d, ...key }) => key
-
 app.get('/.well-known/keys', () => {
-	return { keys: [toPublicKey(sigKey), toPublicKey(encKey)] }
+	return { keys: [sigKey, encKey].map(({ d, ...key }) => key) }
 })
 
 app.get('/url', async (evt) => {
@@ -60,9 +57,10 @@ app.get('/url', async (evt) => {
 		scope: 'openid uinfin name',
 	})
 
-	return { url }
+	return redirect(evt, url)
 })
 
+/** @param {import('openid-client').TokenSet} tokenSet */
 const getUserinfo = async (tokenSet) => {
 	try {
 		return await client.userinfo(tokenSet)
@@ -83,15 +81,15 @@ app.get('/callback', async (evt) => {
 		)
 		console.log(tokenSet.claims())
 		console.log(await getUserinfo(tokenSet))
-		return callbackHtml(`ok`)
+		return html(evt, createHtml(callbackBody(`login ok`)))
 	} catch (err) {
 		console.log(err)
-		return callbackHtml('error during login')
+		return html(evt, createHtml(callbackBody('error during login')))
 	}
 })
 
 app.get('/', (evt) => {
-	return html(evt, indexHtml())
+	return html(evt, createHtml(indexBody))
 })
 
 const server = serve(app, {
